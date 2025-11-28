9fbf61c4e3246e256f7bb975d8bf9c992a5c1f0c
PabloMartinez29 <jpml11292006@gmail.com>
2025-11-28 09:11:33 -0500

    Ultimos arreglos de usuarios y autenticacion

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | These language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | that has failed, such as an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Hemos enviado el enlace de restablecimiento de contraseña a tu correo.',
+    'throttled' => 'Por favor espera antes de volver a intentarlo.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    // Mensaje solicitado cuando el correo no existe en el sistema
+    'user' => 'No existe una cuenta con este correo electrónico.',
+];
+
+
